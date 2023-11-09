f507023b228f973ca57fe58ddcc72dc2d20b726b
Jesus Eduardo Sánchez Daza <91790625+Jsanchez759@users.noreply.github.com>
2023-11-08 20:09:11 -0500

    Update Study_material.py

--- Machine_Learning/temp_quiz_app/pages/Study_material.py
+++ Machine_Learning/temp_quiz_app/pages/Study_material.py
@@ -3,7 +3,7 @@ import PyPDF2
 
 st.title('Study Material for the quiz :books:')
 
-pdf_file_path = "../Introduction to TheJobsDriver.pdf"
+pdf_file_path = "Machine_Learning/temp_quiz_app/Introduction to TheJobsDriver.pdf"
 pdf_file = open(pdf_file_path, 'rb')
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 
